75f9e07175d3a8f2759b61aed2a80b5673984283
anhkatori <anhdoraemon2k@gmail.com>
2023-04-06 17:16:12 +0700

    update layout

---
+++ app/code/Htc/Training/Block/Footer.php
@@ -0,0 +1,14 @@
+<?php
+namespace Htc\Training\Block;
+class Footer extends \Magento\Framework\View\Element\Template
+{
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+
+	public function print()
+	{
+		return __('Test Block');
+	}
+}
